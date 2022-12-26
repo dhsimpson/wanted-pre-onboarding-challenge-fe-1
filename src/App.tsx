aeb3c2c09f5b4cd1e4b9a334eebc49ec6c349ed3
yundonghui <dhsimpson@donnie4567.com>
2022-12-27 00:06:20 +0900

    feat | react-query 를 이용해 회원가입, 로그인 하는 hooks 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { QueryClientProvider, QueryClient } from 'react-query';
 import Main from './pages/main/Main';
 import Auth from './pages/Auth/Auth';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Main />}></Route>
-          <Route path="/auth" element={<Auth />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Main />}></Route>
+            <Route path="/auth" element={<Auth />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
     </div>
   );
 }
--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -1,19 +1,59 @@
 import { TextField, Button, Box, Grid } from '@mui/material';
 import { useState } from 'react';
+import axios from 'axios';
+import { useMutation } from 'react-query';
+
+interface AuthMutationParams {
+  email: string;
+  password: string;
+  url: string;
+}
+
+const validate = (check: (str: string) => boolean, data: string, message: string) => {
+  if (!check(data)) {
+    alert(message);
+    return false;
+  }
+  return true;
+};
+
+const checkEmail = (str: string) => (str.includes('@') && str.includes('.') ? true : false);
+
+const checkPassword = (str: string) => str.length >= 8;
+
+const authApi = (params: AuthMutationParams) =>
+  axios.post(`http://localhost:8080${params.url}`, { email: params.email, password: params.password });
 
 function Auth() {
   const [submitType, setSubmitType] = useState('');
+  const authMutation = useMutation(authApi, {
+    onSuccess: data => {
+      console.log(data);
+    },
+    onError: e => {
+      console.error(e);
+    },
+  });
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    // console.log(submitType);
-    // console.log({
-    //   email: data.get('email'),
-    //   password: data.get('password'),
-    // });
-    // submitType 에 따라 분기처리 (이제부턴 react-query를 이용)
-    // react-query 이용해서, 쿼리 결과에 따라 토큰값 들어 오면 루트 화면으로 이동.
+    const email = data.get('email')?.toString() ?? '';
+    const password = data.get('password')?.toString() ?? '';
+    if (!validate(checkEmail, email, '이메일 형식을 지켜주세요!')) {
+      return;
+    }
+    if (!validate(checkPassword, password, '패스워드 형식을 지켜주세요!')) {
+      return;
+    }
+    if (submitType === 'login') {
+      authMutation.mutate({ email, password, url: '/users/login' });
+      return;
+    }
+    if (submitType === 'signUp') {
+      authMutation.mutate({ email, password, url: '/users/create' });
+      return;
+    }
   };
 
   //머테리얼 ui 참고 : https://github.com/mui/material-ui/blob/v5.11.0/docs/data/material/getting-started/templates/sign-in/SignIn.tsx
