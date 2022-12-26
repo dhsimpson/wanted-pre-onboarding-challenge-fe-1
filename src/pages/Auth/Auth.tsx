970d942153402b590c1645fedc79bc85b3155f52
yundonghui <dhsimpson@donnie4567.com>
2022-12-26 21:22:02 +0900

    feat | 버튼 클릭 시 로그인, 회원가입인 지 체크하도록 hook 추가

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -1,14 +1,19 @@
 import { TextField, Button, Box, Grid } from '@mui/material';
+import { useState } from 'react';
 
 function Auth() {
+  const [submitType, setSubmitType] = useState('');
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log(data);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+    // console.log(submitType);
+    // console.log({
+    //   email: data.get('email'),
+    //   password: data.get('password'),
+    // });
+    // submitType 에 따라 분기처리 (이제부턴 react-query를 이용)
+    // react-query 이용해서, 쿼리 결과에 따라 토큰값 들어 오면 루트 화면으로 이동.
   };
 
   //머테리얼 ui 참고 : https://github.com/mui/material-ui/blob/v5.11.0/docs/data/material/getting-started/templates/sign-in/SignIn.tsx
@@ -37,12 +42,26 @@ function Auth() {
         />
         <Grid container columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
           <Grid item xs>
-            <Button type="submit" fullWidth variant="contained" sx={{ mb: 2 }}>
+            <Button
+              value="login"
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mb: 2 }}
+              onClick={() => setSubmitType('login')}
+            >
               Log In
             </Button>
           </Grid>
           <Grid item xs>
-            <Button type="submit" fullWidth variant="contained" sx={{ mb: 2 }}>
+            <Button
+              value="signUp"
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mb: 2 }}
+              onClick={() => setSubmitType('signUp')}
+            >
               Sign In
             </Button>
           </Grid>
