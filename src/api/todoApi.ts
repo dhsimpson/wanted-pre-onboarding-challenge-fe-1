88d74df2c7ec64d7637553af558e7978bb9a59c8
yundonghui <dhsimpson@donnie4567.com>
2022-12-30 22:32:14 +0900

    feat | todo 수정 기능

--- src/api/todoApi.ts
+++ src/api/todoApi.ts
@@ -17,8 +17,25 @@ interface TodoResponse {
   };
 }
 
+interface UpdateTodoRequest {
+  authToken: string;
+  id: string;
+  title: string;
+  content: string;
+}
+
 export const todosApi = (authToken: string) =>
   axios.get<any, TodosResponse>('http://localhost:8080/todos', { headers: { Authorization: authToken } });
 
 export const todoApi = (authToken: string, id: string) =>
   axios.get<any, TodoResponse>(`http://localhost:8080/todos/${id}`, { headers: { Authorization: authToken } });
+
+export const updateTodoApi = (req: UpdateTodoRequest) =>
+  axios.put(
+    `http://localhost:8080/todos/${req.id}`,
+    {
+      title: req.title,
+      content: req.content,
+    },
+    { headers: { Authorization: req.authToken } },
+  );
--- src/pages/Todos/TodoDetail/TodoDetail.tsx
+++ src/pages/Todos/TodoDetail/TodoDetail.tsx
@@ -1,35 +1,68 @@
 import { useParams } from 'react-router-dom';
-import { useQuery } from 'react-query';
-import { Todo, todoApi } from '../../../api/todoApi';
-import { useState } from 'react';
+import { useQuery, useMutation } from 'react-query';
+import { Todo, todoApi, updateTodoApi } from '../../../api/todoApi';
+import { useRef, useState } from 'react';
 function TodoDetail() {
   const { id } = useParams();
   const authToken = localStorage.getItem('authtoken');
 
   const { data, isLoading, error } = useQuery(['todo', id], () => todoApi(authToken ?? '', id ?? ''));
 
-  const [isModify, setIsModify] = useState(false);
+  const [isUpdate, setIsUpdate] = useState(false);
 
   const todo: Todo | undefined = data?.data?.data ?? undefined;
+  //todo 추가하기
+  //   useMutation()
+
+  //TODO : 수정하기 영역은 컴포넌트화 하고 수정 useMutate 하는 코드도 같이 컴포넌트화
+  //   interface
+
+  //todo 수정하기
+  const titleRef = useRef<HTMLInputElement>(null);
+  const contentRef = useRef<HTMLInputElement>(null);
+
+  const todoUpdateMutation = useMutation(updateTodoApi, {
+    onSuccess: data => {
+      console.log(data);
+      alert('업데이트 완료!');
+    },
+    onError: e => {
+      console.error(e);
+      alert('업데이트 실패!');
+    },
+  });
 
   //TODO : error 시에 alert 및 뒤로가기
   return (
     <div>
-      {isLoading && todo != undefined ? (
+      {isLoading && todo == undefined ? (
         <div>로딩중</div>
       ) : (
         <>
-          {!isModify ? (
+          {!isUpdate ? (
             <div>
               <p>{todo!.title}</p>
               <p>{todo!.content}</p>
-              <button onClick={() => setIsModify(true)}>수정하기</button>
+              <button onClick={() => setIsUpdate(true)}>수정하기</button>
             </div>
           ) : (
             <div>
+              <input type="text" defaultValue={todo!.title} ref={titleRef} />
+              <input type="text" defaultValue={todo!.content} ref={contentRef} />
               수정모드
-              <button onClick={() => setIsModify(false)}>수정완료</button>
-              <button onClick={() => setIsModify(false)}>취소하기</button>
+              <button
+                onClick={() => {
+                  todoUpdateMutation.mutate({
+                    authToken: authToken!,
+                    id: todo!.id,
+                    title: titleRef.current!.value,
+                    content: contentRef.current!.value,
+                  });
+                }}
+              >
+                수정완료
+              </button>
+              <button onClick={() => setIsUpdate(false)}>취소하기</button>
             </div>
           )}
         </>
