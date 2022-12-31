bb40b506c6377fbb23d4ce9a62500f7df9501d76
yundonghui <dhsimpson@donnie4567.com>
2023-01-01 02:03:43 +0900

    feat | todoDetail 의 authToken, todo clean-up

--- src/pages/Todos/TodoDetail/TodoDetail.tsx
+++ src/pages/Todos/TodoDetail/TodoDetail.tsx
@@ -1,20 +1,25 @@
 import { useParams } from 'react-router-dom';
 import { useQuery } from 'react-query';
 import { Todo, todoApi } from 'api/todoApi';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import UpdateTodo from './UpdateTodo';
 import ShowTodo from './ShowTodo';
 function TodoDetail() {
   const { id } = useParams();
-  const authToken = localStorage.getItem('authtoken');
+  let authToken: string | null = localStorage.getItem('authtoken');
 
   const { data, isLoading, error } = useQuery(['todo', id], () => todoApi(authToken ?? '', id ?? ''));
 
   const [isUpdate, setIsUpdate] = useState(false);
 
-  const todo: Todo | undefined = data?.data?.data ?? undefined;
-  //todo 추가하기 기능
-  //   useMutation()
+  let todo: Todo | undefined = data?.data?.data ?? undefined;
+
+  useEffect(() => {
+    return () => {
+      authToken = null;
+      todo = undefined;
+    };
+  });
 
   //TODO : error 시에 alert 및 뒤로가기
   return (
