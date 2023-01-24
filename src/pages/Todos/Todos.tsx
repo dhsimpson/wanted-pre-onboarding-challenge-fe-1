726969491e035516ff543bde5af6354f4e93ffda
yundonghui <dhsimpson@donnie4567.com>
2023-01-25 01:54:20 +0900

    feat | todolist 영역에도 suspense, errorBoundary 처리

--- src/pages/Todos/Todos.tsx
+++ src/pages/Todos/Todos.tsx
@@ -1,40 +1,20 @@
-import { useEffect } from 'react';
-import { useQuery } from 'react-query';
+import { lazy, Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
-import { Todo, todosApi } from 'api/todoApi';
 import AddTodo from 'component/TodoDetail/AddTodo';
-import TodoItem from 'component/TodoDetail/TodoItem';
-import { List } from '@mui/material';
-import { queryDefaultCacheOptions } from 'consts/time';
+import ErrorBoundary from 'component/common/ErrorBoundary';
+const TodoList = lazy(() => import('component/Todos/TodoList'));
 
 function Todos() {
-  const { data, isLoading, error } = useQuery(['todos'], () => todosApi(), queryDefaultCacheOptions);
-  //TODO : error 시에 alert 및 뒤로가기
-
-  let todoList: Todo[] = data?.data?.data ?? [];
-
-  useEffect(() => {
-    return () => {
-      todoList = [];
-    };
-  }, [todoList]);
-
   return (
     <div>
       <div>
         목록 영역
-        {isLoading ? (
-          <div>loading</div>
-        ) : (
-          <>
-            {todoList.map((todo, idx) => (
-              <List key={idx}>
-                <TodoItem todo={todo}></TodoItem>
-              </List>
-            ))}
-          </>
-        )}
-        <AddTodo />
+        <ErrorBoundary>
+          <Suspense fallback={<div>로딩중</div>}>
+            <TodoList />
+          </Suspense>
+          <AddTodo />
+        </ErrorBoundary>
       </div>
       <Outlet></Outlet>
     </div>
